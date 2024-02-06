508431a13fd5a5d19d2c726525d4750e22814161
eugenio socha <eugenio.socha2004@gmail.com>
2024-02-05 23:03:50 -0400

    Initial

---
+++ arithimeticalOperators/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float number1, number2;
+    cout << "Insert a number into the variable: " << endl;
+    cin >> number1;
+
+    cout << "Insert another: " << endl;
+    cin >> number2;
+
+    cout << "Here are the basic operation with the arithimetical operators:" << endl;
+    cout << number1 + number2 << endl;
+    system("PAUSE");    
+    return 0;
+};
